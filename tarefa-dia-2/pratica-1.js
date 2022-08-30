0be7e3c7e265762c2a0457013ec0718c64a8927b
gomesmarcello <marcellogsantos08@gmail.com>
2022-08-30 17:35:24 -0300

    mensagem de commit

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,25 @@
+let nome 
+nome = "Marcello Gomes" 
+
+let  email
+email = "marcellog.santos@hotmail.com"
+
+let profissão
+profissão = "Motoboy"
+
+let idade
+idade = 45
+
+let lugar
+lugar = "Grecia" 
+
+console.log(nome)
+
+console.log(email)
+
+console.log(profissão)
+
+console.log(idade)
+
+console.log(lugar)
+
