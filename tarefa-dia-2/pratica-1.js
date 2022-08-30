491122a3cd96722f69236d14f1aa2b33d14f569e
gomesmarcello <marcellogsantos08@gmail.com>
2022-08-30 18:10:20 -0300

    mensagem de commit

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,21 +1,21 @@
-let nome 
-nome = "Marcello Gomes"
+let nome = "Marcello"
+
 console.log(nome) 
 
-let  email
-email = "marcellog.santos@hotmail.com"
+let  email = "marcellog.santos@hotmail.com"
+
 console.log(email)
 
-let profissão
-profissão = "Motoboy"
+let profissão = "motoboy"
+
 console.log(profissão)
 
-let idade
-idade = 45
+let idade = 45
+
 console.log(idade)
 
-let lugar
-lugar = "Grecia"
+let lugar = "grecia"
+
 console.log(lugar) 
 
 Olá, console.log(nome)! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para console.log(email). Confirma pra gente alguns dados? A sua idade é console.log(idade), você é console.log(profissão) e o seu email é console.log(email) ? Ficamos no aguardo. Muito obrigado e boa viagem! :D
