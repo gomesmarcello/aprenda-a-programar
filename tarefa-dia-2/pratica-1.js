9048e3ef4d26013d06c07cac1be83cb89ad0afcd
gomesmarcello <marcellogsantos08@gmail.com>
2022-08-30 18:02:40 -0300

    mensagem de commit

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -18,7 +18,7 @@ let lugar
 lugar = "Grecia"
 console.log(lugar) 
 
-
+Olá, console.log(nome)! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para console.log(email). Confirma pra gente alguns dados? A sua idade é console.log(idade), você é console.log(profissão) e o seu email é console.log(email) ? Ficamos no aguardo. Muito obrigado e boa viagem! :D
 
 
 
