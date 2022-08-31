8afdb028ec3c409e42055453859469160fe48e8c
gomesmarcello <marcellogsantos08@gmail.com>
2022-08-30 21:59:46 -0300

    mensagem de commit

--- tarefa-dia-3/pratica-2.js
+++ tarefa-dia-3/pratica-2.js
@@ -2,10 +2,11 @@ let multiplicador = 9
 let n = 10
 let i = 1
 
-while (i <=n) {
-    console.log(`${multiplicador} X ${i} = ${multiplicador * 1}`)
+while (i <= n) {
+    let resultado = multiplicador * i
+    let textoDeExibicao = `${multiplicador} X ${i} = ${resultado}`
+    console.log(textoDeExibicao)
     i++
-
 
 
 
